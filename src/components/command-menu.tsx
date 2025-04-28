63dcac4ef7f438a8b1d581f5c2346e29ea974c4c
Daniel Hasdiel <danielhasdiel@gmail.com>
2025-04-28 23:08:56 +0300

    Add dark mode support with next-themes and theme toggle in command menu

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/command";
 import { CommandIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 interface Props {
   links: { url: string; title: string }[];
@@ -20,6 +21,7 @@ interface Props {
 
 export const CommandMenu = ({ links }: Props) => {
   const [open, setOpen] = React.useState(false);
+  const { theme, setTheme } = useTheme();
   let isMac =
     typeof window !== "undefined"
       ? window.navigator.userAgent.indexOf("Mac") > -1
@@ -39,7 +41,7 @@ export const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground xl:block print:hidden">
+      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-white p-1 text-center text-sm text-muted-foreground print:hidden xl:block">
         Press{" "}
         <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">{isMac ? "⌘" : "Ctrl"}</span>+J
@@ -50,7 +52,7 @@ export const CommandMenu = ({ links }: Props) => {
         onClick={() => setOpen((open) => !open)}
         variant="outline"
         size="icon"
-        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl xl:hidden print:hidden"
+        className="fixed bottom-4 right-4 flex rounded-full shadow-2xl print:hidden xl:hidden"
       >
         <CommandIcon className="my-6 size-6" />
       </Button>
@@ -66,6 +68,14 @@ export const CommandMenu = ({ links }: Props) => {
               }}
             >
               <span>Print</span>
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                setOpen(false);
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+            >
+              <span>Change to {theme === "dark" ? "light" : "dark"} mode</span>
             </CommandItem>
           </CommandGroup>
           <CommandGroup heading="Links">
